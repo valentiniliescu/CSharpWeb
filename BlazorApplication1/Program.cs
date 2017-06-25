44a59dfff34fa7b8dc2430a474dd895cbf040025
valenti <valenti@VALENTI-LAPTOP>
2017-06-25 06:52:12 -0700

    Added BlazorApplication1 to source control

---
+++ BlazorApplication1/Program.cs
@@ -0,0 +1,12 @@
+﻿using Blazor.Interop;
+
+namespace BlazorApplication1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Browser.Alert("Hello world!");
+        }
+    }
+}
