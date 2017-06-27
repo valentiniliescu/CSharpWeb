38115300c8f6f84eac5364fb1e556529bc0d869a
valenti <valenti@VALENTI-LAPTOP>
2017-06-27 16:12:55 -0700

    Added demo with HTTP client and JSON deserialization

--- CSharpWebApplication1/Program.cs
+++ CSharpWebApplication1/Program.cs
@@ -1,4 +1,6 @@
 ﻿using CSharpWeb;
+using CSharpWeb.Runtime.Interop;
+using System.Net.Http;
 
 namespace CSharpWebApplication1
 {
@@ -13,11 +15,36 @@ namespace CSharpWebApplication1
             rootDiv.AppendChild(div);
             div.InnerText = "Hello world!";
 
-            var button = new HTMLButtonElement();
-            rootDiv.AppendChild(button);
-            button.InnerText = "Click me!";
+            var button1 = new HTMLButtonElement();
+            rootDiv.AppendChild(button1);
+            button1.InnerText = "Click me!";
 
-            button.Click += () => { div.InnerText = "Hello world again!"; };
+            button1.Click += () => { div.InnerText = "Hello world from button!"; };
+
+            var button2 = new HTMLButtonElement();
+            rootDiv.AppendChild(button2);
+            button2.InnerText = "Load data!";
+
+            button2.Click += async () => {
+                using (var httpClient = new HttpClient())
+                {
+                    var json = await httpClient.GetStringAsync("weather.json");
+                    var weatherForecastArray = JsonUtil.Deserialize<WeatherForecast[]>(json);
+                    div.InnerText = weatherForecastArray[0].ToString();
+                }
+            };
+        }
+    }
+
+    public class WeatherForecast
+    {
+        public string DateFormatted { get; set; }
+        public int TemperatureC { get; set; }
+        public string Summary { get; set; }
+
+        public override string ToString()
+        {
+            return $"Date: {DateFormatted} Temperature: {TemperatureC} Summary: {Summary}";
         }
     }
 }
